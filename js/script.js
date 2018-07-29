df75bbf595f6a246dce54e55d13f2fdfb5970dde
Alysson Campbell <alyssoncampbell252@gmail.com>
2018-07-28 22:04:23 -0400

    worked on movement

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// referenced https://www.sanwebe.com/2011/12/jquery-move-div-leftrightupdown for code
+// https://api.jquery.com/keypress/
+// https://stackoverflow.com/questions/1402698/binding-arrow-keys-in-js-jquery
+// https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+// https://www.quora.com/How-do-you-make-one-div-layer-show-over-another-in-HTML-CSS
+
+$("#gamePiece").on("click", evt => {
+  evt.preventDefault();
+  console.log("clicked!");
+  $("#gamePiece").animate({
+    "marginTop": "+=20px"
+  });
+});
